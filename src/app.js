35a5774564b549fa7e10ad4c711b48a1d7f9405d
Franco Paraván <francoparavan@gmail.com>
2024-06-03 22:41:33 +0000

    Proyecto Generador de Dominios

--- src/app.js
+++ src/app.js
@@ -2,10 +2,28 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+window.onload = () => {
+  const domains = generateDomains();
+  const domainsContainer = document.querySelector("#domains");
+  domains.forEach(domain => {
+    const domainElement = document.createElement("p");
+    domainElement.innerText = domain;
+    domainsContainer.appendChild(domainElement);
+  });
 };
+function generateDomains() {
+  let pronoun = ["the", "our"];
+  let adj = ["great", "big"];
+  let noun = ["jogger", "racoon"];
+  let domains = [];
+
+  for (let i = 0; i < pronoun.length; i++) {
+    for (let j = 0; j < adj.length; j++) {
+      for (let k = 0; k < noun.length; k++) {
+        domains.push(pronoun[i] + adj[j] + noun[k] + ".com");
+      }
+    }
+  }
+
+  return domains;
+}
